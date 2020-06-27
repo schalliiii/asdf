2f7a71da2965a5394107fe2426f8fe700d5083fc
naominoller <naomi.noller@live.de>
2020-06-27 14:43:11 +0200

    Aufgabe 9

---
+++ AUFGABE09/script9.ts
@@ -0,0 +1,45 @@
+// Aufgabe 09 - Hilfe von Herr Rausch und Moritz Labza
+
+// Array erstellen
+
+let todoListe = ["Medienanalyse Präsentation ", "EIA Hausaufgaben ", "Medienpsychologie ", "Medientechnik Präsentation"];
+
+// 
+
+window.addEventListener("load", function () {
+
+    // Ausgabe der Array-Elemente + Anzeige des Papierkorbs
+    
+
+    function drawList() {
+        Liste2.innerHTML = "";
+        for (var index = 0; index < todoListe.length; index++) {
+            Liste2.innerHTML += "<div>" + "<input type='checkbox'>" + todoListe[index] + "<i class='fas fa-trash-alt'id=delete></i>" + "</div>";
+        }
+    
+        // "in total" Anzeige passt sich an
+    
+        var total = document.querySelector("#mySpan");
+        total.innerHTML = todoListe.length;
+    }
+
+    var trash = document.querySelector(".fa-trash-alt");
+    var Liste2 = document.querySelector(".addlist");
+    drawList();
+
+    // Klick auf add-button1 pusht neues Element hinzu
+
+    var input = document.querySelector("#newAdd");
+    console.log(input);
+
+    var addNewElement = document.querySelector("#button1");
+    addNewElement.addEventListener("click", function () {
+        todoListe.push(input.value);
+        drawList();
+        input.value = "";
+        console.log(todoListe);
+    });
+
+});
+
+
